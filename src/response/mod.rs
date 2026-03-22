1821d6c2220380cac589b3083bd258f12c75183d
Zachary Golba <pino@hey.com>
2026-03-22 09:38:08 -0400

    refactor(via): Response::body cannot be accessed after construction (#444)

--- src/response/mod.rs
+++ src/response/mod.rs
@@ -7,117 +7,93 @@ mod file;
 
 pub use body::{Json, ResponseBody};
 pub use builder::{Finalize, ResponseBuilder};
+use delegate::delegate;
 pub use redirect::Redirect;
 
 #[cfg(feature = "file")]
 pub use file::File;
 
-use bytes::Bytes;
 use cookie::CookieJar;
 use http::{Extensions, HeaderMap, StatusCode, Version};
-use http_body::Body;
-use http_body_util::combinators::BoxBody;
 use std::fmt::{self, Debug, Formatter};
 
-use crate::error::BoxError;
-
 pub struct Response {
-    http: http::Response<ResponseBody>,
+    inner: http::Response<ResponseBody>,
     cookies: CookieJar,
 }
 
 impl Response {
     #[inline]
+    pub fn new(body: ResponseBody) -> Self {
+        Self {
+            inner: http::Response::new(body),
+            cookies: CookieJar::new(),
+        }
+    }
+
+    #[inline]
     pub fn build() -> ResponseBuilder {
         Default::default()
     }
 
-    #[inline]
-    pub fn new(body: ResponseBody) -> Self {
-        Self {
-            cookies: CookieJar::new(),
-            http: http::Response::new(body),
+    delegate! {
+        to self.inner() {
+            pub fn status(&self) -> StatusCode;
         }
-    }
 
-    /// Consumes the response returning a new response with body mapped to the
-    /// return type of the provided closure.
-    ///
-    #[inline]
-    pub fn map<U, F>(self, map: F) -> Self
-    where
-        F: FnOnce(ResponseBody) -> U,
-        U: Body<Data = Bytes, Error = BoxError> + Send + Sync + 'static,
-    {
-        Self {
-            cookies: self.cookies,
-            http: self.http.map(|body| BoxBody::new(map(body)).into()),
+        to self.inner_mut() {
+            pub fn status_mut(&mut self) -> &mut StatusCode;
         }
-    }
 
-    #[inline]
-    pub fn status(&self) -> StatusCode {
-        self.http.status()
-    }
+        to self.inner() {
+            pub fn version(&self) -> Version;
+            pub fn headers(&self) -> &HeaderMap;
+        }
 
-    #[inline]
-    pub fn status_mut(&mut self) -> &mut StatusCode {
-        self.http.status_mut()
-    }
-
-    #[inline]
-    pub fn version(&self) -> Version {
-        self.http.version()
-    }
-
-    #[inline]
-    pub fn headers(&self) -> &HeaderMap {
-        self.http.headers()
-    }
-
-    #[inline]
-    pub fn headers_mut(&mut self) -> &mut HeaderMap {
-        self.http.headers_mut()
+        to self.inner_mut() {
+            pub fn headers_mut(&mut self) -> &mut HeaderMap;
+        }
     }
 
     /// Returns a reference to the response cookies.
-    ///
-    #[inline]
     pub fn cookies(&self) -> &CookieJar {
         &self.cookies
     }
 
     /// Returns a mutable reference to the response cookies.
-    ///
-    #[inline]
     pub fn cookies_mut(&mut self) -> &mut CookieJar {
         &mut self.cookies
     }
 
-    #[inline]
-    pub fn extensions(&self) -> &Extensions {
-        self.http.extensions()
+    delegate! {
+        to self.inner() {
+            pub fn extensions(&self) -> &Extensions;
+        }
+
+        to self.inner_mut() {
+            pub fn extensions_mut(&mut self) -> &mut Extensions;
+        }
     }
 
     #[inline]
-    pub fn extensions_mut(&mut self) -> &mut Extensions {
-        self.http.extensions_mut()
+    fn inner(&self) -> &http::Response<ResponseBody> {
+        &self.inner
     }
 
     #[inline]
-    pub fn body(&self) -> &ResponseBody {
-        self.http.body()
+    fn inner_mut(&mut self) -> &mut http::Response<ResponseBody> {
+        &mut self.inner
     }
 }
 
 impl Debug for Response {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         f.debug_struct("Response")
-            .field("version", &self.version())
             .field("status", &self.status())
+            .field("version", &self.version())
             .field("headers", self.headers())
             .field("cookies", &self.cookies)
-            .field("body", self.http.body())
+            .field("body", self.inner.body())
             .finish()
     }
 }
@@ -125,16 +101,16 @@ impl Debug for Response {
 impl From<Response> for http::Response<ResponseBody> {
     #[inline]
     fn from(response: Response) -> Self {
-        response.http
+        response.inner
     }
 }
 
 impl From<http::Response<ResponseBody>> for Response {
     #[inline]
-    fn from(http: http::Response<ResponseBody>) -> Self {
+    fn from(inner: http::Response<ResponseBody>) -> Self {
         Self {
+            inner,
             cookies: CookieJar::new(),
-            http,
         }
     }
 }

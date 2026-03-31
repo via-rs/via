72885613c66137fc879ac74d865347ce8b8a14d2
Zakari Papa <pino@hey.com>
2026-03-31 15:59:11 -0400

    refactor(via): separate predicates from errors in Guard (#469)

--- via/src/guard.rs
+++ via/src/guard.rs
@@ -1,5 +1,8 @@
-use crate::middleware::BoxFuture;
-use crate::{Middleware, Next, Request};
+use crate::{BoxFuture, Middleware, Next, Request};
+
+pub trait Predicate<App> {
+    fn matches(&self, request: &Request<App>) -> bool;
+}
 
 /// Stop processing the request and respond if the provided precondition fails.
 ///
@@ -16,41 +19,79 @@ use crate::{Middleware, Next, Request};
 /// use via::{Request, guard, raise};
 ///
 /// let mut app = via::app(());
+/// let mut api = app.route("/api");
 ///
-/// app.route("/users").scope(|path| {
-///     // Subsequently defined routes require a valid API key.
-///     path.middleware(guard(validate_api_key));
+/// api.middleware(via::guard(
+///     || raise!(401, message = "\"x-api-key\" is missing or invalid."),
+///     |request: &Request<_>| {
+///         request.headers().get("x-api-key").is_some_and(|value| {
+///             todo!("validate api key value");
+///         })
+///     },
+/// ));
+///
+/// // Subsequent routes have a valid API key.
+///
+/// api.route("/users").scope(|users| {
+///     // Define the /api/users resource.
 /// });
-///
-/// fn validate_api_key(request: &Request) -> via::Result<()> {
-///     let Some(key) = request.envelope().headers().get("x-api-key") else {
-///         raise!(401, message = "missing required header: x-api-key");
-///     };
-///
-///     // Insert API key validation here...
-///
-///     Ok(())
-/// }
 /// ```
-pub struct Guard<T> {
-    check: T,
+pub struct Guard<E, T> {
+    or_else: E,
+    predicate: T,
 }
 
-/// Create a guard middleware from the provided check function.
+/// Confirm that request matches the provided predicate before proceeding.
 ///
-pub fn guard<T>(check: T) -> Guard<T> {
-    Guard { check }
+/// # Example
+///
+/// ```rust
+/// use via::{Request, raise};
+///
+/// let mut app = via::app(());
+/// let mut api = app.route("/api");
+///
+/// api.middleware(via::guard(
+///     || raise!(401, message = "\"x-api-key\" is missing or invalid."),
+///     |request: &Request<_>| {
+///         request.headers().get("x-api-key").is_some_and(|value| {
+///             todo!("validate api key value");
+///         })
+///     },
+/// ));
+///
+/// // Subsequent routes have a valid API key.
+///
+/// api.route("/users").scope(|users| {
+///     // Define the /api/users resource.
+/// });
+/// ```
+///
+pub fn guard<E, T>(or_else: E, predicate: T) -> Guard<E, T> {
+    Guard { or_else, predicate }
 }
 
-impl<T, App> Middleware<App> for Guard<T>
+impl<E, T, App> Middleware<App> for Guard<E, T>
 where
-    T: Fn(&Request<App>) -> crate::Result<()> + Send + Sync,
+    E: Fn() -> crate::Result + Send + Sync,
+    T: Predicate<App> + Send + Sync,
 {
     fn call(&self, request: Request<App>, next: Next<App>) -> BoxFuture {
-        if let Err(error) = (self.check)(&request) {
-            Box::pin(async { Err(error) })
-        } else {
+        if self.predicate.matches(&request) {
             next.call(request)
+        } else {
+            let result = (self.or_else)();
+            Box::pin(async { result })
         }
+    }
+}
+
+impl<T, App> Predicate<App> for T
+where
+    T: Fn(&Request<App>) -> bool + Send + Sync,
+{
+    #[inline]
+    fn matches(&self, request: &Request<App>) -> bool {
+        (self)(request)
     }
 }

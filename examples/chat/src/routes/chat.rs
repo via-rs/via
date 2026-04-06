2c4cb7d54758bb441674186f3213d1d94d850315
Zachary Golba <pino@hey.com>
2026-04-06 13:50:04 -0400

    fix(examples/chat): update to the latest API

--- examples/chat/src/routes/chat.rs
+++ examples/chat/src/routes/chat.rs
@@ -1,11 +1,22 @@
 use via::Error;
-use via::ws::{self, Channel, ResultExt};
+use via::ws::{self, Channel, Message, ResultExt};
 
 type Request = ws::Request<crate::Unicorn>;
 
 pub async fn chat(mut channel: Channel, _request: Request) -> ws::Result {
-    while let Some(_message) = channel.recv().await {
-        Err(Error::new("not yet implemented")).or_break()?
+    let fail = || via::raise!(message = "every other message succeeds");
+    let mut n = 0;
+
+    while let Some(message) = channel.recv().await {
+        if let Message::Text(text) = &message {
+            println!("  info(chat): {}", text.as_str());
+        }
+
+        n += 1;
+
+        if n % 2 == 0 {
+            fail().or_reconnect()?;
+        }
     }
 
     Ok(())

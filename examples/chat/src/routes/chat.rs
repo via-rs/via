fae131e61220971ed13ba95088a4fc435ab9126f
Zachary Golba <pino@hey.com>
2026-04-08 08:43:01 -0400

    feat(examples/chat): properly demonstrate ws lifecycle

--- examples/chat/src/routes/chat.rs
+++ examples/chat/src/routes/chat.rs
@@ -1,26 +1,51 @@
+use serde::Deserialize;
 use tokio::task::yield_now;
-use via::ws::{self, Channel, Message, ResultExt};
+use via::Payload;
+use via::ws::{self, Channel, ResultExt};
 
 type Request = ws::Request<crate::Unicorn>;
 
+#[derive(Deserialize)]
+struct Message {
+    body: String,
+}
+
 pub async fn chat(mut channel: Channel, _request: Request) -> ws::Result {
-    let fail = || via::raise!(message = "every other message succeeds");
-    let mut n = 0;
+    if cfg!(debug_assertions) {
+        eprintln!("  info(examples/chat): setup ws recv loop.");
+    }
 
-    while let Some(message) = channel.recv().await {
-        n += 1;
-
-        if n % 2 == 0 {
-            fail().or_reconnect()?;
+    while let Some(next) = channel.recv().await {
+        // Message::is_* methods ensure compatibility with both ws backends.
+        if next.is_close() {
+            break;
         }
 
-        if let Message::Text(text) = &message {
-            println!("  info(chat): {}", text.as_str());
+        if next.is_text() {
+            // The impl of Payload::json for ws::Message does not allocate.
+            // However, fields of the deserialized type can allocate.
+            let message = next.json::<Message>().or_reconnect()?;
+            //                                   ^^^^^^^^^^^^
+            // If an error occurs while deserializing the message due to
+            // malformed user input, restart the session rather than ending it.
+            println!("  info(examples/chat): {}", &message.body);
+        } else if cfg!(debug_assertions) {
+            eprintln!("  warn(examples/chat): ignoring message {:?}", next);
         }
 
-        // Await a future that yields to the runtime in order to uphold of the
-        // contract of the ws reactor.
+        // Yield to the runtime to uphold the web socket reactor contract.
+        //
+        // This isn't necessary if you await any other future that is not
+        // immediately ready.
+        //
+        // For example, sending a reply, acquiring a database connection, or
+        // sending a command to a database unconditionally is enough to
+        // guarantee progress when we're not waiting for I/O.
         yield_now().await;
+    }
+
+    if cfg!(debug_assertions) {
+        eprintln!("  info(examples/chat): ws session ended.");
     }
 
     Ok(())

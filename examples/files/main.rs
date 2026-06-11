9b8e936fe31463d372adebbeaa5473bdd8cfec1c
Zachary Golba <pino@hey.com>
2026-06-11 09:31:42 -0400

    refactor(examples/files): additional cleanup

--- examples/files/main.rs
+++ examples/files/main.rs
@@ -1,13 +1,16 @@
-#![cfg(feature = "fs")]
+#[cfg(not(feature = "file"))]
+compile_error!(concat!(
+    "the \"file\" feature must be enabled to run this example.\n",
+    "re-run this example with: cargo run --example files --features=\"file\""
+));
 
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
 use std::process::ExitCode;
 use std::sync::Arc;
-use std::{env, thread};
 use tokio::sync::Semaphore;
 use via::response::File;
-use via::{Error, Next, Request, ResultExt, Server};
+use via::{Error, Next, Request, Server};
 
 /// The maximum number of connections we are willing to accept before
 /// accounting for resources other than TCP connections.
@@ -23,25 +26,60 @@ const RT_FD_REQUIREMENT: usize = 13;
 
 struct Unicorn {
     /// The directory from which files can be served.
-    public_dir: Arc<Path>,
+    public_dir: Box<Path>,
 
     /// Limit concurrent requests to prevent exit on EMFILE (for safety).
     /// Via prefers that fd back-pressure be a user-space responsibility.
     semaphore: Arc<Semaphore>,
 }
 
+impl Unicorn {
+    fn public_dir(&self) -> &Path {
+        &self.public_dir
+    }
+
+    fn semaphore(&self) -> &Arc<Semaphore> {
+        &self.semaphore
+    }
+}
+
+/// Calculates how many files can be open concurrently and returns it along
+/// with the adjusted maximum number of connections.
+fn determine_resource_usage() -> via::Result<(usize, usize)> {
+    cfg_select! {
+        // On Windows, sockets are not files.
+        windows => Ok((MAX_CONNECTIONS.div_ceil(2), MAX_CONNECTIONS)),
+
+        // On POSIX, max_concurrency affects the max_connections budget.
+        //
+        // The default amount of padding provided by Via is 13. The maximum
+        // amount of concurrent file streams should not exceed the number of
+        // worker process in the tokio runtime.
+        _ => {{
+            let concurrency = std::thread::available_parallelism()?.get();
+            let adjusted_max = MAX_CONNECTIONS - RT_FD_REQUIREMENT - concurrency;
+
+            Ok((concurrency, adjusted_max))
+        }}
+    }
+}
+
+/// Extracts the relative path to the requested file from the request.
+fn extract_file_path(request: &Request<Unicorn>) -> via::Result<PathBuf> {
+    let public_dir = request.app().public_dir();
+
+    if let Some(path_param) = request.param("path").ok()?.as_deref() {
+        Ok(public_dir.join(path_param))
+    } else {
+        Ok(public_dir.join("index.html"))
+    }
+}
+
 /// Resolve a relative path to the public directory.
-///
-/// This example can be run from the workspace root or the examples directory.
-/// Therefore, we must determine the context in which this example was
-/// compiled. Then,
-///
 fn resolve_public_dir() -> PathBuf {
-    const CARGO_MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
 
-    let manifest_dir = Path::new(CARGO_MANIFEST_DIR);
-
-    if CARGO_MANIFEST_DIR.ends_with("examples") {
+    if manifest_dir.ends_with("examples") {
         manifest_dir.join("files/public")
     } else {
         manifest_dir.join("examples/files/public")
@@ -49,28 +87,17 @@ fn resolve_public_dir() -> PathBuf {
 }
 
 async fn serve_dir(request: Request<Unicorn>, _: Next<Unicorn>) -> via::Result {
-    let semaphore = request.app().semaphore.clone();
-    let permit = semaphore.acquire_owned().await?;
-
-    let mut path = {
-        let public_dir = request.app().public_dir.as_ref();
-        let path_param = request
-            .param("path")
-            .into_result()
-            .unwrap_or("index.html".into());
-
-        public_dir.join(path_param.as_ref())
-    };
-
-    let mime_type = if let Some(ext) = path.extension().and_then(OsStr::to_str) {
-        mime_guess::from_ext(ext).first_or_octet_stream()
+    let permit = request.app().semaphore().clone().acquire_owned().await?;
+    let mut path = extract_file_path(&request)?;
+    let content_type = if let Some(extension) = path.extension().and_then(OsStr::to_str) {
+        mime_guess::from_ext(extension).first_or_octet_stream()
     } else {
         path.set_extension("html");
         mime_guess::mime::TEXT_HTML_UTF_8
     };
 
     File::open(path)
-        .content_type(&mime_type)
+        .content_type(&content_type)
         .with_last_modified()
         .permit(permit)
         .serve(1024 * 1024) // stream files > 1 MB
@@ -79,36 +106,10 @@ async fn serve_dir(request: Request<Unicorn>, _: Next<Unicorn>) -> via::Result {
 
 #[tokio::main]
 async fn main() -> Result<ExitCode, Error> {
-    let (max_concurrency, max_connections) = cfg_select! {
-        // On Windows, sockets are not files.
-        windows => (MAX_CONNECTIONS.div_ceil(2), MAX_CONNECTIONS),
-
-        // On POSIX, max_concurrency affects the max_connections budget.
-        //
-        // The default amount of padding provided by Via is 13. The maximum
-        // amount of concurrent file streams should not exceed the number of
-        // worker process in the tokio runtime.
-        _ => {{
-            let concurrency = thread::available_parallelism()?.get();
-            let adjusted_max = MAX_CONNECTIONS - RT_FD_REQUIREMENT - concurrency;
-
-            (concurrency, adjusted_max)
-        }}
-    };
-
-    let public_dir = resolve_public_dir();
-
-    if cfg!(debug_assertions) {
-        println!("serving files from: {:?}", &public_dir);
-        println!(
-            "  max concurrency = {}, max connections = {}",
-            max_concurrency, max_connections
-        );
-    }
-
+    let (max_open_files, max_connections) = determine_resource_usage()?;
     let mut app = via::app(Unicorn {
-        public_dir: public_dir.into(),
-        semaphore: Arc::new(Semaphore::new(max_concurrency)),
+        public_dir: resolve_public_dir().into_boxed_path(),
+        semaphore: Arc::new(Semaphore::new(max_open_files)),
     });
 
     app.route("/*path").to(via::get(serve_dir));

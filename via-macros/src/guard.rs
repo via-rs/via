b595370699a26d1674e6ff963f6b1208d1e46f7d
Zachary Golba <pino@hey.com>
2026-07-12 16:43:26 -0400

    fix(via-macros): target patch, post, and put in content!

--- via-macros/src/guard.rs
+++ via-macros/src/guard.rs
@@ -43,7 +43,11 @@
 macro_rules! content {
     ($accepts:expr, $provides:expr) => {
         via::guard::if_else(
-            via::guard::method::is_mutation(),
+            via::guard::on::method((
+                via::guard::method::patch(),
+                via::guard::method::post(),
+                via::guard::method::put(),
+            )),
             via::guard::on::headers((
                 via::guard::header::accept($provides),
                 via::guard::header::content_type($accepts),

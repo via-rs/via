1bb25f409dcb784889fed946a565227c47b9e14a
Zakari Papa <pino@hey.com>
2026-09-14 14:29:45 -0400

    fix(via): util::once! is general purpose (#661)

--- via/src/util/once.rs
+++ via/src/util/once.rs
@@ -2,10 +2,7 @@
 macro_rules! once {
     ($call:expr) => {
         static __1: std::sync::Once = std::sync::Once::new();
-        __1.call_once(|| {
-            print!("warn: a lossy size hint must be used for RequestBody. ");
-            println!("usize::MAX exceeds u64::MAX on this platform.");
-        });
+        __1.call_once($call);
     };
 }
 
